029f1e8170c13e76e54919116eb46dee7b5998a3
gh-cntests-sb <89937834+gh-cntests-sb@users.noreply.github.com>
2021-09-11 19:54:17 +0530

    initial commit with Spring Boot for Rest Service Integration and Spring Data JPA Integration with DB
    initial commit with Spring Boot for Rest Service Integration and Spring Data JPA Integration with DB

---
+++ src/main/java/xyz/atmlists/atmssearchintegrationservice/AtmsSearchIntegrationServiceApplication.java
@@ -0,0 +1,13 @@
+package xyz.atmlists.atmssearchintegrationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmsSearchIntegrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmsSearchIntegrationServiceApplication.class, args);
+	}
+
+}
